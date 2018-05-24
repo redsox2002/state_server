ed22b17c83ab8b795d649030fb6f1b2d6d9fcaa7
sganes901 <Sudhir_Ganesan2@comcast.com>
2018-05-24 08:08:13 -0400

    Added more comments in code

--- app.js
+++ app.js
@@ -20,12 +20,19 @@ fs.readFile('states.json', 'utf-8', (err, data) => {
 
 
 server.post('/', (req, res) => {
+    // If the lat or long aren't in the request body, throw the error
     if (!req.body.latitude || !req.body.longitude) {
         res.json({
             error: 'lat and long not specified in incoming JSON request'
         });
         res.end();
     }
+    /*
+    If a state is a found, get the lat and long from the states.json file
+    Since states.json is an array of arrays, the first element of the array
+    [0] was the long, and the second [1] was the lat.  This constructs the
+    array that geolib is looking for
+    */
     let foundState;
     states.forEach(state => {
         const stateName = state.state;
@@ -36,6 +43,10 @@ server.post('/', (req, res) => {
                 latitude: border[1]
             });
         });
+    /*
+    The first param for isPointInside is an object with lat, long
+    The second param is an array of objects with polygon points
+    */
         if (geolib.isPointInside({
                 latitude: Number(req.body.latitude),
                 longitude: Number(req.body.longitude)
@@ -43,11 +54,13 @@ server.post('/', (req, res) => {
             foundState = stateName;
         }
     });
+    // If points found, print the state.
     if (foundState) {
         res.json({
             message: `${foundState}`
         });
         res.end();
+    // If not found, print error message
     } else {
         res.json({
             error: 'Lat/Long not found in any state :('
